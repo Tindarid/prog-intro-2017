93b7c6b7c45ae654c58e3c844413a1b20c59d20b
Nursan Valeyev <tindarid@gmail.com>
2019-02-28 16:27:29 +0300

    fix

--- EXAM/java/SimpleFileManager.java
+++ EXAM/java/SimpleFileManager.java
@@ -3,39 +3,60 @@ import java.util.*;
 import java.nio.file.*;
 
 public class SimpleFileManager {
-    public static void main(String [] argc) {
+    public static void main(String [] args) {
         System.out.println("Use \"help\" for more information");
         Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
         try {
             try {
                 Path abs = Paths.get((new File("")).getAbsolutePath());
+                loop:
                 while(in.hasNextLine()) {
                     String [] arguments = in.nextLine().split(" ");
                     DirectoryStream<Path> stream = Files.newDirectoryStream(abs);
-                    if (arguments.length > 2) {
+                    if (arguments.length == 0) {
+                        continue loop;
+                    } else if (arguments.length > 2) {
                         printErrorInfFunc();
-                    } else if(arguments[0].equals("exit")) {
-                        break;
-                    } else if (arguments[0].equals("help")) {
-                        printHelpFunc();
-                    } else if(arguments[0].equals("dir")) {
-                        dirFunc(abs, stream);
-                    } else if(arguments.length != 2) {
-                        printErrorInfFunc();
-                    } else if(arguments[0].equals("rm")) {
-                        rmFunc(arguments[1], stream);
-                    } else if(arguments[0].equals("cd")) {
-                        abs = cdFunc(abs, arguments[1]);
-                    } else if(arguments[0].equals("create")) {
-                        createFunc(abs, arguments[1]);
-                    } else if(arguments[0].equals("mkdir")) {
-                        mkdirFunc(abs, arguments[1]);
-                    } else if(arguments[0].equals("rmdir")) {
-                        rmdirFunc(abs, arguments[1]);
-                    } else {
-                        printErrorInfFunc();
+                        continue loop;
                     } 
-                    System.out.println("------------------------------------------");
+                    String func = arguments[0];
+                    switch (func) {
+                        case "exit":   
+                            break loop;
+                        case "help":   
+                            printHelpFunc();
+                            continue loop;
+                        case "dir":
+                            dirFunc(abs, stream);
+                            continue loop;
+                        default:
+                            if (arguments.length != 2) {
+                                printErrorInfFunc();
+                                continue loop;
+                            } 
+                            break;
+                    }
+                    String arg = arguments[1];
+                    switch (func) {
+                        case "rm":   
+                            rmFunc(arg, stream);
+                            continue loop;
+                        case "cd":
+                            abs = cdFunc(abs, arg);
+                            continue loop;
+                        case "touch":
+                            touchFunc(abs, arg);
+                            continue loop;
+                        case "mkdir":
+                            mkdirFunc(abs, arg);
+                            continue loop;
+                        case "rmdir":
+                            mkdirFunc(abs, arg);
+                            continue loop;
+                        default:
+                            printErrorInfFunc();
+                            break;
+                    }
                 }
 
             } catch (IOException e) {
@@ -47,25 +68,26 @@ public class SimpleFileManager {
     }
     
     private static void printErrorInfFunc() {
-        System.out.println("ERROR, please write correct command(use help command for more information)");
+        System.out.println("ERROR, please write correct command");
+        printHelpFunc();
     }
 
     private static void printHelpFunc() {
-        System.out.println("-exit: close FileManager");
-        System.out.println("-dir: information about current directory");
-        System.out.println("-rm <filename>: delete a file");
-        System.out.println("-cd <directory>: move to directory");
-        System.out.println("-create <filename>: create a file");
-        System.out.println("-mkdir <directory>: create a directory");
-        System.out.println("-rmdir <directory>: delete a directory");
+        System.out.println("exit: close FileManager");
+        System.out.println("dir: information about current directory");
+        System.out.println("rm <filename>: delete a file");
+        System.out.println("cd <directory>: move to directory");
+        System.out.println("create <filename>: create a file");
+        System.out.println("mkdir <directory>: create a directory");
+        System.out.println("rmdir <directory>: delete a directory");
     }
 
     private static void deleteFunc(File file) {
         System.out.println("Deleting: " + file.getAbsolutePath());
-        if(file.isDirectory()) {
+        if (file.isDirectory()) {
             String [] files = file.list();
-            if((files != null) && (files.length != 0)) {
-                for(String f: files) {
+            if (files != null && files.length != 0) {
+                for (String f: files) {
                     deleteFunc(new File(file.getAbsolutePath() + File.separator + f));
                 }
             }
@@ -77,16 +99,12 @@ public class SimpleFileManager {
     private static void rmFunc(String name, DirectoryStream<Path> stream) throws IOException {
         System.out.println("Deleting: " + name);
         boolean flag = false;
-        for(Path p : stream) {
-            if(p.getFileName().toString().equals(name)) {
+        for (Path p : stream) {
+            if (p.getFileName().toString().equals(name)) {
                 flag = Files.deleteIfExists(p);
             }
         }
-        if (flag) {
-            System.out.println("Deleted: " + name);
-        } else {
-            System.out.println("Cannot find a file");
-        }
+        System.out.println((flag ? "Deleted: " : "Cannot find a file ") + name);
     }
 
     private static void mkdirFunc(Path abs, String name) {
@@ -101,7 +119,7 @@ public class SimpleFileManager {
         }
     }
 
-    private static void createFunc(Path abs, String name) {
+    private static void touchFunc(Path abs, String name) {
         Path temp = abs.resolve(name).normalize();
         try {
             Files.createFile(temp);
@@ -123,7 +141,7 @@ public class SimpleFileManager {
     }
 
     private static void dirFunc(Path abs, DirectoryStream<Path> stream) {
-        for(Path p : stream) {
+        for (Path p : stream) {
             System.out.println("\t" + p.getFileName().toString());
         }
         System.out.println("Current directory: " + abs.toString());
